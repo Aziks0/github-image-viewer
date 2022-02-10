2e4cf0ba383e72229fbeadf8e8a4f06423eb9401
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2022-02-10 22:12:51 +0100

    initial commit

---
+++ gh-image.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          github-image-preview
+// @description   Allow README images to be previewed on full monitor size
+// @author        Aziks
+// @version       1.0
+// @license       GPLv3
+// @run-at        document-idle
+// @match         https://github.com/*/*
+// @grant         none
+// ==/UserScript==
+
