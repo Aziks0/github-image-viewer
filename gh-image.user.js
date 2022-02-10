5bccb2303e0f4f02c25d9988f71e0123bbd79263
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2022-02-10 22:13:29 +0100

    feat: add images getter functions

--- gh-image.user.js
+++ gh-image.user.js
@@ -9,3 +9,34 @@
 // @grant         none
 // ==/UserScript==
 
+/**
+ * Get all the image elements from the README
+ *
+ * @returns The image elements contained in the README
+ */
+const getImageElements = () => {
+  const readme = document.getElementById('readme');
+  const article = readme.getElementsByTagName('article')[0];
+  return article.getElementsByTagName('img');
+};
+
+/**
+ * Filter the image elements that are not linked to images
+ *
+ * @param {HTMLImageElement[]} imageElements
+ *
+ * @returns An array containing image elements that are linked to image
+ */
+const filterRepoImageElements = (imageElements) => {
+  return imageElements.filter((imageElement) => {
+    const link = imageElement.parentElement.getAttribute('href');
+    if (!link) return false;
+    return (
+      link.includes('githubusercontent.com') ||
+      link.match('/blob/.*\\.(png|jpe?g)$')
+    );
+  });
+};
+
+};
+
